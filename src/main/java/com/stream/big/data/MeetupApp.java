e579ab5f167e6f0065a76fb9dde1fac7114e2f7e
Vivek <vivekrmd@gmail.com>
2019-05-11 12:47:52 +0530

    Removed comments and unused imports

--- src/main/java/com/stream/big/data/MeetupApp.java
+++ src/main/java/com/stream/big/data/MeetupApp.java
@@ -1,11 +1,7 @@
 package com.stream.big.data;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.socket.client.WebSocketClient;
-import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 
 @SpringBootApplication
 public class MeetupApp {
@@ -14,11 +10,4 @@ public class MeetupApp {
 		SpringApplication.run(MeetupApp.class, args);
 	}
 	
-	/*
-	 * @Bean public ApplicationRunner startup(MeetupWebSocketHandler
-	 * meetupWebSocketHandler) { return args -> { WebSocketClient
-	 * meetupWebSocketClient = new StandardWebSocketClient();
-	 * meetupWebSocketClient.doHandshake(meetupWebSocketHandler, MEETUP_ENDPOINT);
-	 * }; }
-	 */
 }
